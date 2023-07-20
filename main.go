b893a2c4e8ace8c8418e713fd86e135b9d753953
flowerinthenight <flowerinthenight@pm.me>
2023-07-20 12:23:11 +0900

    chore: help

--- main.go
+++ main.go
@@ -30,23 +30,30 @@ Copyright (c) 2023-` + year() + ` Alphaus Cloud, Inc. All rights reserved.
 The general form is ` + bold("tucp <resource[ subresource...]> <action> [flags]") + `.
 
 To authenticate, either set GOOGLE_APPLICATION_CREDENTIALS env var or
-set the --svcacct-file flag. Ask the service owner if your credentials
-file doesn't have access to the service itself.`,
+set the --creds-file flag. You can use the 'iam' tool to request access
+to the 'tucpd-[next|prod]' service, like so:
+
+  $ iam allow-me tucpd-prod
+
+You only need to do this once.`,
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			// dev: tucpd-dev-cnugyv5cta-an.a.run.app
-			// next: tucpd-next-u554nqhjka-an.a.run.app
-			// prod: tucpd-prod-u554nqhjka-an.a.run.app
-			svc := "tucpd-prod-u554nqhjka-an.a.run.app"
+			params.ServiceHost = params.HostProd
+			switch params.RunEnv {
+			case "dev":
+				params.ServiceHost = params.HostDev
+			case "next":
+				params.ServiceHost = params.HostNext
+			}
+
 			ctx := context.Background()
 			var ts oauth2.TokenSource
 			var err error
-
 			switch {
 			case params.CredentialsFile != "":
 				opts := idtoken.WithCredentialsFile(params.CredentialsFile)
-				ts, err = idtoken.NewTokenSource(ctx, "https://"+svc, opts)
+				ts, err = idtoken.NewTokenSource(ctx, "https://"+params.ServiceHost, opts)
 			default:
-				ts, err = idtoken.NewTokenSource(ctx, "https://"+svc)
+				ts, err = idtoken.NewTokenSource(ctx, "https://"+params.ServiceHost)
 			}
 
 			if err != nil {
@@ -61,7 +68,6 @@ file doesn't have access to the service itself.`,
 			}
 
 			params.AccessToken = token.AccessToken
-			logger.Info(params.AccessToken)
 		},
 		Run: func(cmd *cobra.Command, args []string) {
 			logger.Info("see -h for more information")
@@ -72,8 +78,8 @@ file doesn't have access to the service itself.`,
 func init() {
 	rootCmd.Flags().SortFlags = false
 	rootCmd.PersistentFlags().SortFlags = false
-	rootCmd.PersistentFlags().StringVar(&params.CredentialsFile, "svcacct-file", "", "GCP service account file")
-	rootCmd.PersistentFlags().StringVar(&params.AccessToken, "access-token", "", "use directly if not empty")
+	rootCmd.PersistentFlags().StringVar(&params.CredentialsFile, "creds-file", "", "optional, GCP service account file")
+	rootCmd.PersistentFlags().StringVar(&params.RunEnv, "env", "prod", "dev, next, or prod")
 	// rootCmd.AddCommand(
 	// 	cmds.WhoAmICmd(),
 	// )
--- params/params.go
+++ params/params.go
@@ -1,6 +1,14 @@
 package params
 
+const (
+	HostDev  = "tucpd-dev-cnugyv5cta-an.a.run.app"
+	HostNext = "tucpd-next-u554nqhjka-an.a.run.app"
+	HostProd = "tucpd-prod-u554nqhjka-an.a.run.app"
+)
+
 var (
 	CredentialsFile string // service acct file for GCP access
-	AccessToken     string // use directly if not empty
+	AccessToken     string // auto-set, use as Bearer in subcommands
+	RunEnv          string // dev, next, prod (default)
+	ServiceHost     string // auto-set
 )
